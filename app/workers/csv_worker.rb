a4e6bb12d8b69d63eee994c3e5df1ef72d66dbb2
Adarsh Chandran <adarsh.c@fayausa.com>
2019-07-23 04:04:01 +0530

    Add comment for csv_worker method

--- app/workers/csv_worker.rb
+++ app/workers/csv_worker.rb
@@ -3,6 +3,7 @@ class CsvWorker
     sidekiq_options retry: false
 
     def perform(items)
+        # Using Activerecord-import to bulk import to importers in single query
         Importer.import(items)
     end
 end
